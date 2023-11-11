8cb8bf55e0f451bac09df68ce51db310831f0bcd
hüseyin <138565952+azadd847@users.noreply.github.com>
2023-11-11 13:20:38 +0300

    ...
    ...

--- alisveriss3/app.js
+++ alisveriss3/app.js
@@ -1,69 +1,72 @@
 const express = require("express");
-const mysql = require("mysql");
 const cors = require("cors");
 const app = express();
 const dotenv = require("dotenv");
 const path = require("path");
-
+const api = require("./api/api");
+const bodyParser = require("body-parser");
 dotenv.config({ path: '.env' });
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'alisveris'
-});
 
-db.connect((error) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("MySQL connection");
-    }
-});
 
 // Body parsing middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.json());
 
 // Use cors middleware
 app.use(cors());
 
+app.use("/index", (req, res) => {
+   // console.log(req)
+    //console.log("hi")
+     const { action } = req.body;
+     if (api[action]) {
+       api[action](req, res);
+     } else {
+       res.status(404).send("Invalid action");
+     }
+   });
 // Insert a test user (you can remove this in production)
-app.get("/insert-test-user", (req, res) => {
-    const query = "INSERT INTO users (username, password) VALUES ('testuser', 'testpassword')";
-    db.query(query, (err, result) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send("Internal Server Error");
-            return;
-        }
-        res.status(200).send("Test user inserted successfully");
-    });
-});
+// app.get("/insert-test-user", (req, res) => {
+//     const query = "INSERT INTO users (username, password) VALUES ('testuser', 'testpassword')";
+//     db.query(query, (err, result) => {
+//         if (err) {
+//             console.error(err);
+//             res.status(500).send("Internal Server Error");
+//             return;
+//         }
+//         // res.status(200).send("Test user inserted successfully");
+//         res.status(200).json({ message: "Login successful", username: kullaniciAdi });
 
-app.post("/login", (req, res) => {
-    const { kullaniciAdi, parola } = req.body;
-    console.log("Received login request:", req.body);
+//     });
+// });
 
-    // Perform your MySQL query to check the credentials
-    const query = "SELECT * FROM users WHERE username = ? AND password = ?";
-    db.query(query, [kullaniciAdi, parola], (err, result) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send("Internal Server Error");
-            return;
-        }
+// app.post("/login", (req, res) => {
+//     const { kullaniciAdi, parola } = req.body;
+//     console.log("Received login request:", req.body);
 
-        if (result.length > 0) {
-            // User authenticated successfully
-            res.status(200).send("Login successful");
-        } else {
-            // Incorrect username or password
-            res.status(401).send("Invalid credentials");
-        }
-    });
-});
+//     // Perform your MySQL query to check the credentials
+//     const query = "SELECT username FROM users WHERE username = ? AND password = ?";
+//     db.query(query, [kullaniciAdi, parola], (err, result) => {
+//         console.log("11");
+//         if (err) {
+//             console.log(err);
+//             res.status(500).send("Internal Server Error");
+//             return;
+//         }
+//         console.log(result);
+//         if (result.length > 0) {
+//             console.log(result);
+//             // User authenticated successfully
+//             //res.status(200).json({ message: "Login successful", username: kullaniciAdi });
+//             res.status(200).send(result)
+//         } else {
+//             // Incorrect username or password
+//             res.status(401).send("Invalid credentials");
+//         }
+//     });
+// });
 
 // Static dosyaları servis etmek için public klasörünü kullan
 app.use(express.static(path.join(__dirname, "public")));
--- alisveriss3/public/login/user.js
+++ alisveriss3/public/login/user.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", async function () {
     console.log("DOMContentLoaded event fired");
 
     const loginForm = document.getElementById("login-form");
-    const loginLink = document.getElementById("navbar-login");
+    const loginLink = document.querySelector("#navbar-login");
 
     if (!loginForm || !loginLink) {
         console.error("Login form or login link not found.");
@@ -11,37 +11,76 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     loginForm.addEventListener("submit", async (event) => {
         event.preventDefault();
-
+        console.log("executed login function")
         const kullaniciAdi = document.getElementById("kullaniciAdi").value;
         const parola = document.getElementById("parola").value;
+        console.log(kullaniciAdi, parola)
 
         try {
-            const response = await fetch("http://localhost:5050/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    kullaniciAdi,
-                    parola,
-                }),
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            
+            var raw = JSON.stringify({
+              "action": "Login",
+              "kullaniciAdi": kullaniciAdi,
+              "password": parola
             });
-
-            if (!response.ok) {
-                console.error("Login failed:", response.statusText);
-                return;
-            }
-
-            console.log("Login successful. Updating UI and redirecting.");
-            console.log("Before update - InnerText:", loginLink.innerText);
-            loginLink.innerText = "Hesabım";
-            console.log("After update - InnerText:", loginLink.innerText);
             
-                window.location.replace("../index.html");
+            var requestOptions = {
+              method: 'POST',
+              headers: myHeaders,
+              body: raw,
+              redirect: 'follow'
+            };
             
+            fetch("http://localhost:5050/index", requestOptions)
+              .then(response => response.json())
+              .then(result => {
+                console.log(result[0])
+                localStorage.setItem("auth", result[0].username);
+            //  loginLink.innerHTML = `<i class="fas fa-user"></i> ${result.username}`
+             window.location.replace("../home.html")
+                })
+              .catch(error => console.log('error', error));
+
 
         } catch (error) {
             console.error("An error occurred:", error);
         }
     });
+
+    function updateNavbarContent() {
+      const authUsername = localStorage.getItem("auth");
+      if (authUsername) {
+          loginLink.innerHTML = `<i class="fas fa-user"></i> ${authUsername}`;
+          navbarLogin.removeAttribute("href");
+          createDropdown(authUsername);
+        }
+  }
+  function createDropdown(username) {
+    const dropdownContainer = document.createElement("div");
+    dropdownContainer.classList.add("dropdown");
+
+    const dropdownButton = document.createElement("button");
+    dropdownButton.innerHTML = `<i class="fas fa-user"></i> ${username} <i class="fas fa-caret-down"></i>`;
+    dropdownButton.classList.add("dropbtn");
+    dropdownContainer.appendChild(dropdownButton);
+
+    const dropdownContent = document.createElement("div");
+    dropdownContent.classList.add("dropdown-content");
+    const logoutLink = document.createElement("a");
+    logoutLink.href = "#";
+    logoutLink.innerHTML = "Logout";
+    logoutLink.addEventListener("click", () => {
+        localStorage.removeItem("auth");
+        window.location.replace("../index.html");
+    });
+    dropdownContent.appendChild(logoutLink);
+
+    dropdownContainer.appendChild(dropdownContent);
+
+    navbarLogin.parentNode.replaceChild(dropdownContainer, navbarLogin);
+}
+
+  updateNavbarContent();
 });
