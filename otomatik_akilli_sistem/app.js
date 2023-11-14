042b7b892ad136bcc5b6cf92d3f685cbf0da1b90
hüseyin <138565952+azadd847@users.noreply.github.com>
2023-11-14 10:26:03 +0300

    ...
    ...

---
+++ otomatik_akilli_sistem/app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const dotenv = require("dotenv");
+const path = require("path");
+const api = require("./api/api");
+const bodyParser = require("body-parser");
+dotenv.config({ path: '.env' });
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use("/index", (req, res) => {
+  
+     const { action } = req.body;
+     if (api[action]) {
+       api[action](req, res);
+     } else {
+       res.status(404).send("Invalid action");
+     }
+   });
+// Insert a test user (you can remove this in production)
+// app.get("/insert-test-user", (req, res) => {
+//     const query = "INSERT INTO users (username, password) VALUES ('testuser', 'testpassword')";
+//     db.query(query, (err, result) => {
+//         if (err) {
+//             console.error(err);
+//             res.status(500).send("Internal Server Error");
+//             return;
+//         }
+//         // res.status(200).send("Test user inserted successfully");
+//         res.status(200).json({ message: "Login successful", username: kullaniciAdi });
+
+//     });
+// });
+
+// app.post("/login", (req, res) => {
+//     const { kullaniciAdi, parola } = req.body;
+//     console.log("Received login request:", req.body);
+
+//     // Perform your MySQL query to check the credentials
+//     const query = "SELECT username FROM users WHERE username = ? AND password = ?";
+//     db.query(query, [kullaniciAdi, parola], (err, result) => {
+//         console.log("11");
+//         if (err) {
+//             console.log(err);
+//             res.status(500).send("Internal Server Error");
+//             return;
+//         }
+//         console.log(result);
+//         if (result.length > 0) {
+//             console.log(result);
+//             // User authenticated successfully
+//             //res.status(200).json({ message: "Login successful", username: kullaniciAdi });
+//             res.status(200).send(result)
+//         } else {
+//             // Incorrect username or password
+//             res.status(401).send("Invalid credentials");
+//         }
+//     });
+// });
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(5050, () => {
+    console.log("Server port 5050'de çalışıyor");
+});
